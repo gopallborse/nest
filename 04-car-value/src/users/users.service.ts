2373e389cf9f6092e2c18b960c594d6b0e72ae1f
Gopall Borse <gopallborse@gmail.com>
2025-03-30 19:11:53 +0530

    user route working

--- 04-car-value/src/users/users.service.ts
+++ 04-car-value/src/users/users.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  //   repo: Repository<User>;
+
+  //   constructor(repo: Repository<User>) {
+  //     this.repo = repo;
+  //   }
+
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id }); // to find a single entity by its properties.
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } }); // to find multiple entities i.e. [] by their properties.
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+    return this.repo.remove(user);
+  }
+}
